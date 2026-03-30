e951ff65162a507fc6ff7e08c040b56a8ad0086d
abkr020 <abkr020@gmail.com>
2026-03-30 23:31:55 +0530

    select * insted of specifying minimal thing i wnat every thing in the token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -238,7 +238,7 @@ export const ssoLogin = async (req, res) => {
 
     // ✅ 1. Fetch user from PostgreSQL
     const result = await neonQuery(
-      "SELECT id, name, email, password FROM users WHERE email = $1",
+      "SELECT * FROM users WHERE email = $1",
       [email]
     );
 
